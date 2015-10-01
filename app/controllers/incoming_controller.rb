cde13c6c68fc4bd738d9ef9cb94f27661c6ca00a
Ketan Shukla <ketankshukla@gmail.com>
2015-10-01 12:17:59 -0700

    Test1 - Testing different paras on Heroku to make incoming email bookmarks work

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,22 +5,30 @@ class IncomingController < ApplicationController
 
   def create
 
+    @user = User.find_by(email: params[:sender])
+    @topic = Topic.find_by(title: params[:subject])
+
+    @url = params["body-plain"]
+
+    if @user.nil?
+      @user = User.new(email: params[:sender], password: "password")
+      @user.skip_confirmation!
+      @user.save!
+    end
+
+    if @topic.nil?
+      @topic = @user.topics.create(title: params[:subject])
+    end
+
+    @bookmark = @topic.bookmarks.create(url: @url)
+
+    head 200
+  end
+
+=begin
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
     Rails.logger.info "INCOMING PARAMS HERE: #{params}"
-
-    # You put the message-splitting and business
-    # magic here.
-
-    # Find the user by using params[:sender]
-    # Find the topic by using params[:subject]
-    # Assign the url to a variable after retreiving it from params["body-plain"]
-
-    # Check if user is nil, if so, create and save a new user
-
-    # Check if the topic is nil, if so, create and save a new topic
-
-    # Now that you're sure you have a valid user and topic, build and save a new bookmark
 
     @user = User.find_by(email: params[:sender])
     Rails.logger.info "User: #{@user.inspect}"
@@ -34,9 +42,7 @@ class IncomingController < ApplicationController
       @bookmark.save
     end
 
-
-    # Assuming all went well.
     head 200
+=end
 
-  end
 end
